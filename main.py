07683aefebd2e9a98aaece4135369b826e0f2d80
RaheeqMousa <raheeqmousa00@gmail.com>
2026-09-07 13:18:54 +0000

    Add Command-Line argumant parsing and integrated OpenRouter

---
+++ main.py
@@ -0,0 +1,45 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+import argparse
+
+def main():
+	load_dotenv()
+	api_key=os.environ.get("OPENROUTER_API_KEY")
+	if api_key is None:
+		raise RuntimeError("Open ROUTERAPI key is not found")
+
+	client= OpenAI(
+		base_url="https://openrouter.ai/api/v1",
+		api_key=api_key,
+	)
+	
+	parser= argparse.ArgumentParser(description="Chatbot")
+	parser.add_argument("user_prompt", type=str, help="User prompt")
+	parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+
+	args= parser.parse_args()
+	
+	messages=[
+		{
+		"role":"user",
+		"content": args.user_prompt
+		}
+	]
+
+	response = client.chat.completions.create(
+		model="openrouter/free",
+ 		messages=messages
+	)
+
+	if response.usage is None:
+		raise RuntimeError("API response udage is unavailable")
+
+	if args.verbose: 
+		print(f"User prompt: {args.user_prompt}")
+		print(f"Prompt tokens: {response.usage.prompt_tokens}")
+		print(f"Response tokens: {response.usage.completion_tokens}")
+	print(response.choices[0].message.content)
+
+if __name__ =="__main__":
+	main()
