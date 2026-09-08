d802a94d17a767279282bff89857ce0a5f56a027
RaheeqMousa <raheeqmousa00@gmail.com>
2026-09-08 14:37:51 +0000

    Implement get_file_content, write_file, and run_python_file

---
+++ functions/write_file.py
@@ -0,0 +1,24 @@
+import os
+
+def write_file(working_directory:str, file_path:str, content:str) -> str:
+	try:
+		working_dir_abs= os.path.abspath(working_directory)
+
+		target_path= os.path.normpath(
+			os.path.join(working_dir_abs, file_path)
+		)
+
+		is_valid_path= os.path.commonpath(
+			[working_dir_abs, target_path]) == working_dir_abs
+
+		if not is_valid_path:
+			return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+		if os.path.isdir(target_path):
+			return f'Error: Cannot write to "{file_path}" as it is a directory'
+
+		with open(target_path, "w") as f:
+			f.write(content)
+
+		return f'Successfully wrote to "{target_path}" ({len(content)} characters written)'
+	except Exception as ex:
+		return f"Error: {ex}"
