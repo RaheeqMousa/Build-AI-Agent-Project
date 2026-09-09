8a782529bb7551f77db68f223186ab74f53edde9
RaheeqMousa <raheeqmousa00@gmail.com>
2026-09-09 10:32:10 +0000

    functions call

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI
 import argparse
+from prompt import system_prompt
+import json
+from call_function import available_functions, call_function
 
 def main():
 	load_dotenv()
@@ -13,33 +16,44 @@ def main():
 		base_url="https://openrouter.ai/api/v1",
 		api_key=api_key,
 	)
-	
+
 	parser= argparse.ArgumentParser(description="Chatbot")
 	parser.add_argument("user_prompt", type=str, help="User prompt")
 	parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 
 	args= parser.parse_args()
-	
+
 	messages=[
-		{
-		"role":"user",
-		"content": args.user_prompt
-		}
+		{"role":"system","content": system_prompt},
+		{"role":"user", "content": args.user_prompt}
 	]
 
 	response = client.chat.completions.create(
 		model="openrouter/free",
- 		messages=messages
+ 		messages=messages,
+		temperature=0,
+		tools=available_functions,
 	)
+	
+	message= response.choices[0].message
+	if message.tool_calls:
+		for tool_call in message.tool_calls:
+			result_message= call_function(tool_call)
 
+			if not result_message.get("content"):
+				raise RuntimeError("Tool call returned empty content")
+
+			if args.verbose:
+				print(f"-> {result_message['content']}")
+	else:
+		print(message.content)
 	if response.usage is None:
-		raise RuntimeError("API response udage is unavailable")
+		raise RuntimeError("API response usage is unavailable")
 
 	if args.verbose: 
 		print(f"User prompt: {args.user_prompt}")
 		print(f"Prompt tokens: {response.usage.prompt_tokens}")
 		print(f"Response tokens: {response.usage.completion_tokens}")
-	print(response.choices[0].message.content)
 
 if __name__ =="__main__":
 	main()
